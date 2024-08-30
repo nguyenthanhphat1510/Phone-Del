656ac22837e7ffc9adf0b57db8a52fd9402ce921
nguyenthanhphat1510 <nguyenthanhphat1510@gmail.com>
2024-08-30 22:45:22 +0700

    build menu

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ExploreMenu.css'
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes.</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+            return (
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                    <img src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+
+export default ExploreMenu
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+const Home = () => {
+
+    const [category,setCategory] = useState("A11");
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
+  )
+}
+
+export default Home
